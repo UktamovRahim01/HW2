2c9ddb473b47d855ea16e9294377a2d7769a1c6f
UktamovRahim01 <121957549+UktamovRahim01@users.noreply.github.com>
2023-03-31 15:59:15 +0500

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,8 @@
+
+
+// let students = [1,2,3,'Farrux', true, 'hello ', ['guys', 'my', 11], true, 'name is ', [[0,'years old',0,'and I am ']]]
+// console.log(students[5] + students[6][0]+ ` ` + students[6][1]+ ` `  + students[8] + students[3] + ` ` +  students[9][0][3] + students[6][2] + ` ` + students[9][0][1]);
+
+
+
+
